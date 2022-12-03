7a69b7379c623abc1ef5d391bcd0981892ed653f
STConan <wellingtonverdugamacias@gmail.com>
2022-12-03 05:55:29 +0000

    v0.2
    Signed-off-by: STConan <wellingtonverdugamacias@gmail.com>

--- main.cpp
+++ main.cpp
@@ -6,24 +6,21 @@
 #include "mbed.h"
 #include <iostream>
 
-// Blinking rate in milliseconds
 #define RATE     500ms
 
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-    DigitalOut StatusLED(PA_3);
-    AnalogIn Potentiometer(PA_6);
-    AnalogOut DimmableLED(PA_5);
+    DigitalOut StatusLED(PA_3); //Light to indicate program functioning
+    AnalogIn Potentiometer(PA_6); //Input potentiometer to dimm LED
+    AnalogOut DimmableLED(PA_5); //LED to be dimmed
 
     while (true) {
-        StatusLED.write(1);
-        float DimmFactor = Potentiometer.read();
+        StatusLED.write(1); //Turn on status LED
+        float DimmFactor = Potentiometer.read(); //Storing the reading from the potentiometer
         cout << "Potentiometer reading = " << (DimmFactor * 3.3) << " volts\n";
         cout << "Dimm Factor = " << (DimmFactor * 100.0) << "%\n";
-        cout << DimmFactor << endl;
-        if (DimmFactor >= 0.8){
+        if (DimmFactor >= 0.8){ //Setting up the dimm based on the reading from potentiometer
             DimmableLED.write(1.0);
         } else if (DimmFactor >= 0.6) {
             DimmableLED.write(0.8);
